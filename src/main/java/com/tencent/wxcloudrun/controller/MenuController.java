3a0236693ac4cf487b803c56e166aa9dbf0202b4
cs.chen <cs.chen@tpv-tech.com>
2022-07-28 16:40:02 +0800

    add getMessage api

--- src/main/java/com/tencent/wxcloudrun/controller/MenuController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/MenuController.java
@@ -1,13 +1,22 @@
 package com.tencent.wxcloudrun.controller;
 
+import java.util.HashMap;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api/menu")
+@Slf4j
 public class MenuController {
 	
 	private static RestTemplate restTemplate;
@@ -20,7 +29,28 @@ public class MenuController {
 	@GetMapping("/create")
 	public static String createMenu() {
 		String url="https://api.weixin.qq.com/cgi-bin/menu/create";
-		
+		log.info("create menu:{}",url);
 		return getRequestEntity(url);
+	}
+	@PostMapping("/msg")
+	public static String getMessage() {
+		String url="https://api.weixin.qq.com/customservice/msgrecord/getmsglist";
+		log.info("get msg:{}",url);
+		
+//      设置请求头，请求类型为json
+      HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(MediaType.APPLICATION_JSON);
+
+//      设置请求参数
+      HashMap<String, Object> map = new HashMap<>();
+      map.put("starttime", 987654321);
+      map.put("endtime",987654321);
+      map.put("msgid",1);
+      map.put("number",100);
+
+      //用HttpEntity封装整个请求报文
+      HttpEntity<HashMap<String, Object>> request = new HttpEntity<>(map, headers);
+
+		return restTemplate.postForObject(url, request,String.class);	
 	}
 }
