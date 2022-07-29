fd8cc618b39a4759707a7c159a7b53f5dde76117
cs.chen <cs.chen@tpv-tech.com>
2022-07-29 10:45:39 +0800

    initial restTemplate

--- src/main/java/com/tencent/wxcloudrun/controller/MenuController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/MenuController.java
@@ -22,6 +22,9 @@ public class MenuController {
 	private static RestTemplate restTemplate;
 	
 	private static String getRequestEntity(String url) {
+		if(restTemplate==null)
+			restTemplate=new RestTemplate();
+		
 		ResponseEntity<String> entity=restTemplate.getForEntity(url, String.class);
 		return entity.getBody();
 	}	
@@ -51,17 +54,14 @@ public class MenuController {
       //用HttpEntity封装整个请求报文
       HttpEntity<HashMap<String, Object>> request = new HttpEntity<>(map, headers);
 
+		if(restTemplate==null)
+			restTemplate=new RestTemplate();
+      
 		return restTemplate.postForObject(url, request,String.class);	
 	}
 	@GetMapping("/test")
 	public static String feedbackMsg() {
 		log.info("test : do nothing");
 		return "test ok!";
-	}
-	
-	@GetMapping("/bd")
-	public static String startBaidu() {
-		log.info("start baidu test");
-		return getRequestEntity("www.baidu.com");
 	}
 }
