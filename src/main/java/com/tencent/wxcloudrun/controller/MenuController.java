6e5af7dc86b08e32f1ce5fe94031962b4c663487
cs.chen <cs.chen@tpv-tech.com>
2022-07-29 17:34:26 +0800

    add recv

--- src/main/java/com/tencent/wxcloudrun/controller/MenuController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/MenuController.java
@@ -2,6 +2,9 @@ package com.tencent.wxcloudrun.controller;
 
 import java.util.HashMap;
 
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -15,7 +18,7 @@ import org.springframework.web.client.RestTemplate;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/menu")
+@RequestMapping("/api")
 @Slf4j
 public class MenuController {
 	
@@ -26,6 +29,9 @@ public class MenuController {
 			restTemplate=new RestTemplate();
 		
 		ResponseEntity<String> entity=restTemplate.getForEntity(url, String.class);
+		
+		log.info("response:{}",entity.toString());
+		
 		return entity.getBody();
 	}	
 	//资源复用时，需要额外携带调用方的AppId，否则就会报错：api unauthorized rid: 62e34bba-083ae8bb-5b9f65c1
@@ -39,30 +45,12 @@ public class MenuController {
 		log.info("create menu:{}",url);
 		return getRequestEntity(url);
 	}
-	@PostMapping("/msg")
-	public static String getMessage() {
-		String url="https://api.weixin.qq.com/customservice/msgrecord/getmsglist";
-		url=encodeAppID(url);
-		log.info("get msg:{}",url);
+	@PostMapping("/recv")
+	public static String onMessageEvent() {
 		
-//      设置请求头，请求类型为json
-      HttpHeaders headers = new HttpHeaders();
-      headers.setContentType(MediaType.APPLICATION_JSON);
-
-//      设置请求参数
-      HashMap<String, Object> map = new HashMap<>();
-      map.put("starttime", 987654321);
-      map.put("endtime",987654321);
-      map.put("msgid",1);
-      map.put("number",100);
-
-      //用HttpEntity封装整个请求报文
-      HttpEntity<HashMap<String, Object>> request = new HttpEntity<>(map, headers);
-
-		if(restTemplate==null)
-			restTemplate=new RestTemplate();
-      
-		return restTemplate.postForObject(url, request,String.class);	
+		
+		
+		return "success";
 	}
 	@GetMapping("/test")
 	public static String feedbackMsg() {
@@ -77,4 +65,42 @@ public class MenuController {
 		return getRequestEntity(url);
 	}	
 	
+	@GetMapping("/getip")
+	public static String getwechatIp() {
+		String url="https://api.weixin.qq.com/cgi-bin/get_api_domain_ip";
+		String iplist=getRequestEntity(url);
+		try {
+			JSONObject jsondata=new JSONObject(iplist);
+			
+			
+			JSONArray jsonarray=jsondata.optJSONArray("ip_list");
+			
+			iplist=jsonarray.toString();
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return iplist;
+	}
+	@GetMapping("/getcallbackip")
+	public static String getCallbackIp() {
+		String url="https://api.weixin.qq.com/cgi-bin/getcallbackip";
+		String iplist=getRequestEntity(url);
+		try {
+			JSONObject jsondata=new JSONObject(iplist);
+			
+			
+			JSONArray jsonarray=jsondata.optJSONArray("ip_list");
+			
+			iplist=jsonarray.toString();
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return iplist;		
+	}
 }
